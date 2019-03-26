81d635a87778b7951ad098fcf01bfbcaac1f9c4d
Slade <austinwasinger@gmail.com>
2019-03-26 15:46:34 -0600

    added blobmanager

---
+++ XopolyCore/Logic/BlobManager.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.IO;
+
+namespace XopolyCore.Logic
+{
+    public class BlobManager
+    {
+        private CloudBlobContainer blobContainer;
+
+        public BlobManager(string ContainerName)
+        {
+            // Check if Container Name is null or empty  
+            if (string.IsNullOrEmpty(ContainerName))
+            {
+                throw new ArgumentNullException("ContainerName", "Container Name can't be empty");
+            }
+            try
+            {
+                // Get azure table storage connection string.  
+                string ConnectionString = "Your Azure Storage Connection String goes here";
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+
+                CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+                blobContainer = cloudBlobClient.GetContainerReference(ContainerName);
+
+                // Create the container and set the permission  
+                if (blobContainer.CreateIfNotExistsAsync().Result)
+                {
+                    blobContainer.SetPermissionsAsync(
+                        new BlobContainerPermissions
+                        {
+                            PublicAccess = BlobContainerPublicAccessType.Blob
+                        }
+                    ).Wait();
+                }
+            }
+            catch (Exception ExceptionObj)
+            {
+                throw ExceptionObj;
+            }
+        }
+
+        public string UploadText(string text)
+        {
+            string AbsoluteUri;
+            // Check HttpPostedFileBase is null or not  
+            if (string.IsNullOrWhiteSpace(text))
+                return null;
+
+            try
+            {
+                CloudBlockBlob blockBlob;
+                // Create a block blob  
+                blockBlob = blobContainer.GetBlockBlobReference($"{DateTime.Now}");
+                // Set the object's content type  
+                blockBlob.Properties.ContentType = "application/json";
+                // upload to blob  
+                blockBlob.UploadTextAsync(text);
+
+                // get file uri  
+                AbsoluteUri = blockBlob.Uri.AbsoluteUri;
+            }
+            catch (Exception ExceptionObj)
+            {
+                throw ExceptionObj;
+            }
+            return AbsoluteUri;
+        }
+
+        //public List<string> BlobList()
+        //{
+        //    List<string> _blobList = new List<string>();
+        //    foreach (IListBlobItem item in blobContainer.ListBlobsSegmentedAsync())
+        //    {
+        //        if (item.GetType() == typeof(CloudBlockBlob))
+        //        {
+        //            CloudBlockBlob _blobpage = (CloudBlockBlob)item;
+        //            _blobList.Add(_blobpage.Uri.AbsoluteUri.ToString());
+        //        }
+        //    }
+        //    return _blobList;
+        //}
+
+        public bool DeleteBlob(string AbsoluteUri)
+        {
+            try
+            {
+                Uri uriObj = new Uri(AbsoluteUri);
+                string BlobName = Path.GetFileName(uriObj.LocalPath);
+
+                // get block blob refarence  
+                CloudBlockBlob blockBlob = blobContainer.GetBlockBlobReference(BlobName);
+
+                // delete blob from container      
+                blockBlob.DeleteAsync();
+                return true;
+            }
+            catch (Exception ExceptionObj)
+            {
+                throw ExceptionObj;
+            }
+        }
+    }
+}
---
+++ XopolyCore/Logic/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XopolyCore.Logic
+{
+    public class Logger
+    {
+        BlobManager _blobManager;
+
+        public Logger(BlobManager blobManager)
+        {
+            _blobManager = blobManager;
+        }
+
+        public void Log(Exception ex)
+        {
+            string logMessage = $"[{DateTime.Now}] {ex}";
+            _blobManager.UploadText(logMessage);
+        }
+    }
+}
